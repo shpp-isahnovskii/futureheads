f327e0eee08193b6863ed5e042ce43145adfa134
noty-ivan <ivan.sakhnovskyi@noty.ai>
2024-05-15 12:26:43 +0300

    switchd scrolling text animation to the pure css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin');
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
@@ -15,14 +18,33 @@ export default {
         '3xl': '1800px',
       },
       animation: {
-        'running-text': 'forward-n-back 1s ease-in-out Infinite',
+        'running-forward': 'forward-n-back 60s ease-in-out Infinite alternate',
+        'running-backward': 'back-n-forward 70s ease-in-out Infinite alternate',
       },
       keyframes: {
+        'forward-n-back': {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%) translateX(100vw)' },
+        },
         'back-n-forward': {
-          '100%': {transform: 'translateX(-100%)' }
-        }
-      }
+          '0%': { transform: 'translateX(-100%) translateX(100vw)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ matchUtilities, addUtilities }) => {
+      matchUtilities({
+        'animation-duration': (value) => {
+          return {
+            'animation-duration': value,
+          };
+        },
+      });
+      addUtilities({
+        '.animation-paused': { 'animation-play-state': 'paused' },
+      });
+    }),
+  ],
 };
